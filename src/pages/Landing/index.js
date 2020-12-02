f9b14701fd619f28df6864bba63b3ac07ade46da
hishamss <hishamss90@gmail.com>
2020-12-02 13:40:10 -0700

    added and styled the login form in the landing page

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,10 +1,29 @@
 import "./style.css";
-import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, Form, Button } from "react-bootstrap";
 function Landing() {
   return (
     <div id="landing-container">
-      <Card>
-        <Card.Body>This is some text within a card body.</Card.Body>
+      <Card id="login-card">
+        <Card.Body>
+          <h3>Welcome to Shoprite</h3>
+          <Form id="login-form">
+            <Form.Group controlId="login-username">
+              <Form.Control type="text" placeholder="Enter Username" required />
+            </Form.Group>
+            <Form.Group controlId="login-password">
+              <Form.Control
+                type="password"
+                placeholder="Enter Password"
+                required
+              />
+            </Form.Group>
+            <br />
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+          </Form>
+        </Card.Body>
       </Card>
     </div>
   );
