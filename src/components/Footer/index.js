d5e9148d38cdf2b71c80de0925eb24eb526e0418
hishamss <hishamss90@gmail.com>
2020-12-09 14:07:08 -0700

    added footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+const Footer = () => {
+  return (
+    <div id="main-footer">
+      <p style={{ margin: "0", fontSize: "1.2rem" }}>
+        Shoprite Inc. &copy; 2020
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
